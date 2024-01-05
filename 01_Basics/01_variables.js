1b7eba70ee7b7a1e2bdf4bc112175cde3345af88
Pranav-Goel07 <138761739+Pranav-Goel07@users.noreply.github.com>
2024-01-05 07:28:53 +0000

    Done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+const account_Id = 1445532; // To declare constant
+let accountEmail = "hitesh@google.com"; // To Declare Variables
+var accountPassword = "12345";  // To Declare Variables
+accountCity = "Jaipur";
+
+let account_State;
+// account_Id = 22; // Constant value not change because it is constant
+
+accountEmail = "Pranavgoel@gmail.com";
+accountPassword = "54321";
+accountCity = "banglore"
+
+// console.log(account_Id);
+// console.log(accountEmail);
+
+console.table([account_Id,accountEmail,accountPassword,accountCity,account_State]);
